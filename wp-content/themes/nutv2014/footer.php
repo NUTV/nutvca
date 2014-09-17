83d27240a99e20d862f405420e2bf1a5346fa27b
Jonathan Prince <jonathan@gmail.com>
2014-09-17 17:54:32 +0200

    create/wp-content/themes/nutv2014

---
+++ wp-content/themes/nutv2014/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package nutv
+ */
+?>
+
+	</section><!-- #content -->
+
+	<footer id="colophon" class="site-footer clear" role="contentinfo">
+
+	</footer><!-- #colophon -->
+</section><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
