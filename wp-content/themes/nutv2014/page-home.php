285769b3e1a7a10b263169b9e40ac3071a5389e9
Jonathan Prince <jonathan.prince@gmail.com>
2014-10-13 11:34:30 +0200

    remove sharing links from home page

--- wp-content/themes/nutv2014/page-home.php
+++ wp-content/themes/nutv2014/page-home.php
@@ -22,15 +22,6 @@ get_header( 'home' );
 
 <?php endwhile; // end of the loop. ?>
 
-<div id="social-media-icons">
-	<span class='st_email_large' displayText='Email'></span>
-	<span class='st_facebook_large' displayText='Facebook'></span>
-	<span class='st_linkedin_large' displayText='LinkedIn'></span>
-	<span class='st_twitter_large' displayText='Tweet'></span>
-	<span class='st_googleplus_large' displayText='Google +'></span>
-	<span class='st_pinterest_large' displayText='Pinterest'></span>
-</div>
-
 </section>
 
 
