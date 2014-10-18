2b7bece1429e1b59381d322583733726f1844161
Jonathan Prince <jonathan.prince@gmail.com>
2014-10-18 22:51:26 +0200

    add sponsor links to home page

--- wp-content/themes/nutv2014/page-home.php
+++ wp-content/themes/nutv2014/page-home.php
@@ -22,14 +22,29 @@ get_header( 'home' );
 
 <?php endwhile; // end of the loop. ?>
 
-<div id="social-media-icons">
-	<span class='st_email_large' displayText='Email'></span>
-	<span class='st_facebook_large' displayText='Facebook'></span>
-	<span class='st_linkedin_large' displayText='LinkedIn'></span>
-	<span class='st_twitter_large' displayText='Tweet'></span>
-	<span class='st_googleplus_large' displayText='Google +'></span>
-	<span class='st_pinterest_large' displayText='Pinterest'></span>
-</div>
+<?php
+
+	$sponsors = get_bookmarks( array('category_name' => 'Sponsors'));
+
+	if (count($sponsors) > 0) {
+
+		echo '<div id="sponsors">';
+
+		foreach ($sponsors as $sponsor) {
+			
+			echo '<a href="'.$sponsor->link_url.'" target="'.$sponsor->link_target.'">';
+			if ($sponsor->link_image !== '') {
+				echo '<img src="'.$sponsor->link_image.'" />';
+			} else {
+				echo $sponsor->link_name;
+			}
+			echo '</a>';
+		}
+
+		echo '</div>';
+	}
+
+?>
 
 </section>
 
