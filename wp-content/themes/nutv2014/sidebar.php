aaad9a7d53f719ea921196fbb4c063b4f40eecd6
Jonathan Prince <jonathan.prince@gmail.com>
2014-10-30 19:48:07 +0100

    update sidebar with correct twitter url

--- wp-content/themes/nutv2014/sidebar.php
+++ wp-content/themes/nutv2014/sidebar.php
@@ -13,7 +13,7 @@
 	
 	<section class="social-media-links">
 		<a href="https://www.facebook.com/NUTV.CA" target="blank" title="NUTV on Facebook" class="sm-facebook">NUTV on Facebook</a>
-		<a href="https://twitter.com/NUTVatUofC" target="blank" title="NUTV on Twitter" class="sm-twitter">NUTV on Twitter</a>
+		<a href="https://twitter.com/NUTV_UofC" target="blank" title="NUTV on Twitter" class="sm-twitter">NUTV on Twitter</a>
 		<a href="https://www.youtube.com/user/NUTV" target="blank" title="NUTV YouTube Channel" class="sm-youtube">NUTV YouTube Channel</a>
 		<a href="https://plus.google.com/106514053690782313670" target="blank" title="NUTV on Google+" class="sm-plus">NUTV on Google+</a>
 	</section>
