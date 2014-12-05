b3be5171d99a8e152d9ccb89ad3e95a64742e567
Richard Walker <digitalsadhu@gmail.com>
2014-12-06 00:19:28 +1300

    Fix missing http://

--- wp-content/themes/nutv2014/header.php
+++ wp-content/themes/nutv2014/header.php
@@ -31,7 +31,7 @@
         <nav class="contain">
             <ul>
                 <li><a href="/">home</a></li>
-                <li><a href="shows.nutv.ca">shows</a></li>
+                <li><a href="http://shows.nutv.ca">shows</a></li>
                 <li><a href="/events">events</a></li>
                 <li><a href="/wanna-make-tv">join</a></li>
                 <li><a href="/contact">contact</a></li>
